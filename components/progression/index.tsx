9c260c33de4c1a6e224b0ec53e95f84ce66ae779
Yazdun <yazduntube@gmail.com>
2022-06-05 17:10:34 +0430

    general : made progress

--- components/progression/index.tsx
+++ components/progression/index.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import css from './styles.module.css'
+import { TbPlayerSkipForward, TbPlayerSkipBack } from 'react-icons/tb'
+import Link from 'next/link'
 
 export const Progression: React.FC = () => {
-  return <div>Progression</div>
+  return (
+    <div className={css.wrapper}>
+      <div className={css.container}>
+        <p className={css.artist}>
+          Starry Night
+          <span>Vincent Van Gogh</span>
+        </p>
+        <div className={css.actions}>
+          <Link href="/">
+            <a>
+              <TbPlayerSkipBack />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <TbPlayerSkipForward />
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
