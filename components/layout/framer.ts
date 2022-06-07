9a3e6a318e072aace1682dfb210cf6fd0af16848
Yazdun <yazduntube@gmail.com>
2022-06-07 16:13:26 +0430

    framer : updated

--- components/layout/framer.ts
+++ components/layout/framer.ts
@@ -2,5 +2,5 @@ export const framer_variants = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { duration: 0.5 },
+  transition: { duration: 0.3 },
 }
