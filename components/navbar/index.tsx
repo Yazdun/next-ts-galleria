b8b843a71c8498849de7f60ff808839ec1e7016b
Yazdun <yazduntube@gmail.com>
2022-06-07 11:03:10 +0430

    mobile nav : updated

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -9,56 +9,29 @@
 import { project_logo } from '@/assets/shared'
 import Image from 'next/image'
 import React from 'react'
-import { useWindowSize } from '@/hooks/index'
-import {
-  PROJECT_LOGO_MOBILE_SIZES,
-  PROJECT_LOGO_DESKTOP_SIZES,
-} from '@/constants/index'
 import css from './styles.module.css'
 import Link from 'next/link'
 import { data } from '@/data/index'
 import { useRouter } from 'next/router'
-import { AnimatePresence, motion } from 'framer-motion'
-import { framer_variants } from './framer'
 
 export const Navbar: React.FC = () => {
-  const { isTouch } = useWindowSize()
   const router = useRouter()
   const isHome = router?.pathname === '/'
 
   return (
     <nav className={css.nav}>
       <ul className={css.list}>
-        <li>
-          <Image
-            src={project_logo}
-            alt="galleria"
-            width={
-              isTouch
-                ? PROJECT_LOGO_MOBILE_SIZES.width
-                : PROJECT_LOGO_DESKTOP_SIZES.width
-            }
-            height={
-              isTouch
-                ? PROJECT_LOGO_MOBILE_SIZES.height
-                : PROJECT_LOGO_DESKTOP_SIZES.height
-            }
-          />
+        <li className={css.image}>
+          <Image src={project_logo} alt="galleria" />
         </li>
         <li>
           <Link href={isHome ? data[0].slug : '/'}>
             <a className={css.link}>
-              <AnimatePresence exitBeforeEnter>
-                {isHome ? (
-                  <motion.span {...framer_variants} key="stop">
-                    start
-                  </motion.span>
-                ) : (
-                  <motion.span {...framer_variants} key="start">
-                    stop
-                  </motion.span>
-                )}
-              </AnimatePresence>{' '}
+              {isHome ? (
+                <span key="stop">start</span>
+              ) : (
+                <span key="start">stop</span>
+              )}{' '}
               slideshow
             </a>
           </Link>
